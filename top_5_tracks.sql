a5ffd78154fcd1113f61792e6b37a921d970b724
Jeremy Bakker <jeremy.t.bakker@gmail.com>
2017-05-01 14:09:27 -0500

    Update top_5_tracks.sql

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -2,6 +2,6 @@ SELECT t.Name, il.TrackId, COUNT(il.TrackId) as Count
 FROM InvoiceLine as il
 LEFT JOIN Track t
 ON t.TrackId = il.TrackId
-GROUP BY il.TrackId
+GROUP BY t.Name
 ORDER BY Count DESC
 LIMIT 5
